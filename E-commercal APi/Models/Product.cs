f80e798b9c26e67ff0bbc19abdf7a55ecb4735a2
kemo-108 <kemo26800@gmail.com>
2026-07-17 10:07:21 +0300

    بودي بقبقوتي

--- E-commercal APi/Models/Product.cs
+++ E-commercal APi/Models/Product.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-
+//s
 namespace E_commercal_APi.Models
 {
     [Index(nameof(Code), IsUnique = true)]
